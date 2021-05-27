7c8a27e96f2b4e6140f0be558cba660099707016
Robert <robert@MacBook-Pro-Robert.local>
2021-05-27 14:19:56 +0200

    add layouts

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-app.use('/user', (req, res, next) => {
- res.show('forbidden.html');
- next();
-});
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
-app.get('/home', (req, res) => {
-  res.show('index.html');
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark'});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
-});
+  res.status(404).send('404 not found...');
+})
 
-app.listen(3000, () => {
-  console.log('Server is running on port: 3000');
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
 });
