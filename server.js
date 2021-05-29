77b764e23f4c2833a1b49ae68a0082fe9548b6b8
Robert <robert@MBP-Robert.home>
2021-05-29 16:44:00 +0200

    add file upload

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer({ dest: "uploads/" });
 
 const app = express();
+
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -31,6 +37,22 @@ app.get('/info', (req, res) => {
 app.get('/history', (req, res) => {
   res.render('history');
 });
+
+
+app.post('/contact/send-message', upload.single('fileName'), (req, res) => {
+  try {
+    const { author, sender, title, message } = req.body;
+    const { fieldname, originalname } = req.file;
+
+    if (author && sender && title && message && fieldname) {
+      res.render('contact', { isSent: true, originalname });
+    }
+  } catch (error) {
+    res.render("contact", { isError: true });
+  }
+});
+
+
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
